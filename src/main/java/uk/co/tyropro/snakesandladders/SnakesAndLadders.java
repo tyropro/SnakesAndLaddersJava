38d888398c93bc5459c236489db39f9c6cd94822
tyropro <tyroprocodes@gmail.com>
2023-09-03 00:44:02 +0100

    optimised main file

--- src/main/java/uk/co/tyropro/snakesandladders/SnakesAndLadders.java
+++ src/main/java/uk/co/tyropro/snakesandladders/SnakesAndLadders.java
@@ -22,7 +22,7 @@ public class SnakesAndLadders {
 	
 
 	// check if player has landed on a space with a snake or a ladder
-	static Integer CheckMovement(Integer space) {
+	static int CheckMovement(int space) {
 		// logic for snakes and ladders
 		if (board[space-1] != 0 ) {
 			if (board[space-1] < space) { // logic for snake
@@ -31,46 +31,41 @@ public class SnakesAndLadders {
 				System.out.println("You have landed on a ladder!");
 			}
 			space = board[space-1];
-			System.out.println("You are now on space " + space.toString() + ".");
-			return space; // returns new space 
-		} else {
-			return space;
-		}
-	}
+			System.out.println("You are now on space " + space + ".");
+        }
+        return space; // returns new space
+    }
 	
 	// check if player has won
-	static boolean CheckWin(Integer int_space) {
-		if (int_space == 100) { // logic for if lands on 100
-			return true;
-		} else {
-			return false;
-		}
+	static boolean CheckWin(int int_space) {
+        // logic for if lands on 100
+        return int_space == 100;
 	}
 	
 	// check if player is over goal
-	static Integer CheckOverGoal(Integer int_space) {
-		if (int_space > 100) { // logic for if goes over 100 
-			Integer int_spacesOver = int_space - 100; // finds out how many spaces over 100
+	static int CheckOverGoal(int int_space) {
+		if (int_space > 100) { // logic for if it goes over 100
+			int int_spacesOver = int_space - 100; // finds out how many spaces over 100
 			int_space -= int_spacesOver * 2; // moves the player back how many spaces they have gone over
 											 // (*2 necessary because *1 would put player on 100)
 			
-			System.out.println("You have rolled " + int_spacesOver.toString() + " spaces over 100.");
-			System.out.println("You are now on space " + int_space.toString() + ".");
+			System.out.println("You have rolled " + int_spacesOver + " spaces over 100.");
+			System.out.println("You are now on space " + int_space + ".");
 		}
 		return int_space;
 	}
 	
-	static Integer Turn(Integer int_space) {
+	static int Turn(int int_space) {
 		// roll a dice
 		double dbl_roll = Math.floor(Math.random() * 6) + 1;
-		Integer int_roll = (int)dbl_roll;
+		int int_roll = (int)dbl_roll;
 		
 		// increment current space by dice roll
 		int_space += int_roll;
 		
 		// output dice roll and current space
-		System.out.println("You rolled a " + int_roll.toString() + ".");
-		System.out.println("You are now on space " + int_space.toString() + ".");
+		System.out.println("You rolled a " + int_roll + ".");
+		System.out.println("You are now on space " + int_space + ".");
 		
 		return int_space;
 	}
@@ -85,10 +80,10 @@ public class SnakesAndLadders {
 			boolean bool_play = true;	
 			
 			// player's space on board
-			Integer int_space = 1;
+			int int_space = 1;
 			
 			// count the number of turns the user takes
-			Integer int_turns = 0;
+			int int_turns = 0;
 
 			// code loop
 			while (bool_play) {
@@ -121,7 +116,7 @@ public class SnakesAndLadders {
 			}
 			
 			// converts 'turns' to string
-			String str_turns = int_turns.toString();
+			String str_turns = Integer.toString(int_turns);
 			
 			// prints you win message
 			System.out.println("You win!!!\nIt took " + str_turns + " turns!");
